adf50c10473e893570c42a2249e9c7fe85e14307
alexsmoreno <68883789+alexsmoreno@users.noreply.github.com>
2021-08-20 01:48:13 -0500

    appV1

---
+++ src/main/java/src/main/java/com/uns/demo/CeidunsAppApplication.java
@@ -0,0 +1,14 @@
+package src.main.java.com.uns.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeidunsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeidunsAppApplication.class, args);
+	
+	}
+
+}
