c6e9ff79d5a2781cbfc697408f733ae345944843
alexsmoreno <68883789+alexsmoreno@users.noreply.github.com>
2021-08-21 12:25:01 -0500

    aa

--- src/main/java/models/example.java
+++ src/main/java/models/example.java
@@ -1,8 +1,18 @@
 package models;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
-public class example {
+public class example  {
+
+   
  // .........java -
+    
+    
+    
+    public int getonw(){
+       return 1;
+    }
 }
