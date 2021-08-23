1dd88cf6df541909bfd7e87586c16757e63bd5b7
alexsmoreno <68883789+alexsmoreno@users.noreply.github.com>
2021-08-23 13:04:38 -0500

    funciona

--- src/main/java/controller/AulasController.java
+++ src/main/java/controller/AulasController.java
@@ -6,32 +6,35 @@
 package controller;
 
 import entity.Aula;
-import service.AulaServiceImplement;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.AulaServiceImplement;
 
-import org.springframework.ui.Model;
 /**
  *
  * @author aguss
  */
-@Controller 
-@RequestMapping("/views/aulas")
-public class aulaController {
-   
+  @Controller 
+
+  @RequestMapping("/views/aulas")
+public class AulasController {
+    
     @Autowired
     private  AulaServiceImplement aulaService;
-    @GetMapping("/")
-    public String getListAula(Model model){
-       // List<Aula> listadoAula = aulaService.listarAula();
-      //  model.addAttribute("lisAulas",listadoAula);
+    @GetMapping("/listar")
+    public  String getAula(Model model){
+        List<Aula> listadoAula = aulaService.listarAula();
+        model.addAttribute("lisAulas",listadoAula);
         
-        return "/views/aulas/listar";
+       return "/views/aulas/listar";
+       // return "index";
+       // return listadoAula.toString();
     }
-    
     
 }
--- src/test/java/com/uns/demo/CeidunsAppApplicationTests.java
+++ src/test/java/com/uns/demo/CeidunsAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.uns.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+
 class CeidunsAppApplicationTests {
 
 	@Test
